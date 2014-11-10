d1958c85b03ff1e55a999ac4805d99a64e60586c
redice524 <redice_dragon@hotmail.com>
2014-11-10 18:35:39 +0000

    GPIO.c GPIOconfig input output feitos (talvez)

--- sources/Lab4/GPIO.c
+++ sources/Lab4/GPIO.c
@@ -2,6 +2,25 @@
 #include "LPC2xxx.h"
 #include "LPC2106.h"
 
-void GPIO_config(unsigned int dir, unsigned int mask, unsigned int initvalue);
-void GPIO_output(unsigned int value);
-unsigned int GPIO_input(unsigned int mask); 
+GPIO_DSC gpio;
+
+void GPIO_config(unsigned int dir, unsigned int mask, unsigned int initvalue){
+		if(dir)//Set pins in mask to GPIO function Output 
+		{
+			gpio.IODIR |= mask; 
+			gpio.IOPIN= ((gpio.IOPIN & ~mask ) |initvalue);
+			
+		}else //Set pins in mask to GPIO function Input
+		{
+			gpio.IODIR &= ~mask;
+		}
+		
+}
+
+void GPIO_output(unsigned int value, int mask){
+	gpio.IOPIN= (gpio.IOPIN & ~mask) | value;
+}
+
+unsigned int GPIO_input(unsigned int mask){
+	return gpio.IOPIN & mask;
+}
