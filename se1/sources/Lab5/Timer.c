ff13cf36ba3a78e674d5542244bb79edf6405dd4
brunofins <bfins@alunos.isel.ipl.pt>
2014-12-05 14:58:10 +0000

    Timer lab5

---
+++ se1/sources/Lab5/Timer.c
@@ -0,0 +1,21 @@
+#include "Timer.h"
+
+/* Iniciação do sistema para permitir o acesso ao periférico Timer 0. O timer
+é iniciado em modo continuo e à frequência indicada no parâmetro frequency. */
+void TMR0_Init(unsigned int frequency){
+	
+	
+}
+
+/* Devolve o valor corrente do Timer 0 em unidades de contagem (ticks). */
+unsigned int TMR0_GetValue(void){
+	
+	
+}
+
+/* Devolve o valor, em unidades de contagem (ticks), desde o lastRead até o
+tempo corrente. */
+unsigned int TMR0_Elapsed(unsigned int lastRead){
+	
+	
+}
