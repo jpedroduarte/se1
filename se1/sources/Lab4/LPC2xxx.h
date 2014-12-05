2a1ee775156c0bf4486473a6799ddbbe15c183a3
brunofins <bfins@alunos.isel.ipl.pt>
2014-12-05 13:42:20 +0000

    LPC2xxx.h

--- se1/sources/Lab4/LPC2xxx.h
+++ se1/sources/Lab4/LPC2xxx.h
@@ -1,11 +1,29 @@
 #ifndef LPC2xxx_h
 #define LPC2xxx_h
 
+typedef volatile unsigned int LPC2xxx_REG;
+
 typedef struct{
-	volatile unsigned int IOPIN;
-	volatile unsigned int IOSET;
-	volatile unsigned int IODIR;
-	volatile unsigned int IOCLR;
+	LPC2xxx_REG IOPIN;
+	LPC2xxx_REG IOSET;
+	LPC2xxx_REG IODIR;
+	LPC2xxx_REG IOCLR;
 } GPIO_DSC;
+
+typedef struct {
+	/* Miscellaneous Register Group */
+	LPC2xxx_REG ILR, CTC, CCR, CIIR, AMR;
+	LPC2xxx_REG CTIME0, CTIME1, CTIME2;
+	/* Time Counter Group */
+	LPC2xxx_REG SEC, MIN, HOUR;
+	LPC2xxx_REG DOM, DOW, DOY, MONTH, YEAR;
+	LPC2xxx_REG RESERVED[8]; /* Dummy */
+	/* Alarm Register Group */
+	LPC2xxx_REG ALSEC, ALMIN, ALHOUR, ALDOM, ALDOW, ALDOY, ALMON, ALYEAR;
+	/* Reference Clock Divider */
+	LPC2xxx_REG PREINT, PREFRAC;
+} LPC210X_RTC;
+
+
 
 #endif 
