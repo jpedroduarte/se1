d3bebe78069a9b475a1c938c3736a420748e2692
brunofins <bfins@alunos.isel.ipl.pt>
2014-12-13 18:38:26 +0000

    LAB5 Timer

---
+++ se1/sources/Lab5/LPC2106.h
@@ -0,0 +1,20 @@
+#ifndef LPC2106_h
+#define LPC2106_h
+
+/****************************************************************************\
+| CLOCK DEFINITIONS FOR LPC2106
+\****************************************************************************/
+#define LPC2106_MAIN_OSC 14745600 // Main #000000
+#define LPC2106_CCLK LPC2106_MAIN_OSC // Output PLL Clock
+#define LPC2106_PCLK LPC2106_CCLK/4 // Peripheral Clock
+
+/****************************************************************************\
+| BASE ADDRESS DEFINITIONS FOR LPC2106
+\*********************************************#000000#000000*******************************/
+#define GPIO_base 0xE0028000 
+#define GPIO (*((GPIO_DSC *)GPIO_base))
+
+#define TIMER0 ( *( (LPC210X_TC *) 0xE0004000) )
+#define TIMER1 ( *( (LPC210X_TC *) 0xE0008000) )
+
+#endif
--- se1/sources/Lab5/Timer.c
+++ se1/sources/Lab5/Timer.c
@@ -3,19 +3,35 @@
 /* Iniciação do sistema para permitir o acesso ao periférico Timer 0. O timer
 é iniciado em modo continuo e à frequência indicada no parâmetro frequency. */
 void TMR0_Init(unsigned int frequency){
-	
-	
+	if(frequency > LPC2106_PCLK) return;
+	TIMER0.IR= 0x00000000;
+	TIMER0.TCR= 0x00000000;
+	TIMER0.TC;							//Overflow counter
+	TIMER0.PR= LPC2106_PCLK/frequency; 	//Maximum value possible for the Prescale Counter
+	TIMER0.PC; 							//Prescale Counter
+	TIMER0.MCR=	0x0000000;				//Match Control Register- Feature Disabled
+	TIMER0.MR0;							//Match Register- Not used
+	TIMER0.MR1;							//Match Register- Not used
+	TIMER0.MR2;							//Match Register- Not used
+	TIMER0.MR3;							//Match Register- Not used
+	TIMER0.CCR= 0x00000000;				//Capture Control Register - Feature Disabled
+	TIMER0.CR0;							//Capture Register
+	TIMER0.CR1;							//Capture Register
+	TIMER0.CR2;							//Capture Register
+	TIMER0.CR3;							//Capture Register
+	TIMER0.EMR= 0x00000000;				//External Match Register- Do Nothing
+	TIMER0.RESERVED[12]; 				/* Dummy */
+	TIMER0.CTCR= 0x0 | 0x1; 			//Counter mode: TC incremented every rising edge
 }
 
 /* Devolve o valor corrente do Timer 0 em unidades de contagem (ticks). */
 unsigned int TMR0_GetValue(void){
-	
-	
+	return TIMER0.TC;
 }
 
 /* Devolve o valor, em unidades de contagem (ticks), desde o lastRead até o
 tempo corrente. */
 unsigned int TMR0_Elapsed(unsigned int lastRead){
-	
-	
+	 unsigned int now= TMR0_GetValue();
+	 return now-lastRead;
 }
