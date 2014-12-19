5af5e5da472e75b43f60e005c38c462c56dda55e
brunofins <bfins@alunos.isel.ipl.pt>
2014-12-19 15:34:33 +0000

    keyboard

--- se1/sources/Lab4/keyboard.c
+++ se1/sources/Lab4/keyboard.c
@@ -2,8 +2,8 @@
 #include "GPIO.h"
 
 //mudar as mascaras do config para aqui
-unsigned int col_mask  = 0x000000F0;
-unsigned int line_mask = 0x0000000F;
+unsigned int col_mask  = 0x00000F00;
+unsigned int line_mask = 0x000000F0;
 unsigned int keys[16];
 char key_idx[] = { 0xFF, 0, 1, 0xFF, 2, 0xFF, 0xFF, 0xFF ,3};
 
@@ -22,24 +22,26 @@ void KBD_Init(unsigned int layout[16]){
 /* Devolve 1 se existe alguma tecla pressionada e 0 no caso contrário. Não é
 bloqueante. */
 int KBD_Hit(void){
-	int res = GPIO_input() & line_mask;
+	unsigned input = GPIO_input();
+	int res = input & line_mask;
 	
-	return res;
+	return res != 0;
 }
 
 /* Devolve o código da tecla pressionada. É bloqueante. */
 int KBD_Read(void){
-	unsigned int i, init_col = 0x10, last_col = 0x80;
+	unsigned int i, init_col = 0x100, last_col = 0x800;
 	unsigned int line, aux, col;
 	
 	for(i=init_col;  ; i<<=1){
-		if(i == last_col)
+		if(i > last_col)
 			i=init_col;
 		GPIO_output(i, i); //injetar 1 na coluna
 		if(KBD_Hit()){
-			int aux_line = GPIO_input() & line_mask;
-			line = key_idx[GPIO_input() & line_mask];
-			col = key_idx[i>>4];
+			unsigned in = (GPIO_input() & line_mask)>>4;
+			line = key_idx[in];
+			unsigned c = i>>8;
+			col = key_idx[c];
 			return keys[4 * line + col];
 		}
 		GPIO_output(0, i);
