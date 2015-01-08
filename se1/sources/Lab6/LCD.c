e04fa1481f1b893b00c0166f3fae98aeaaac1491
brunofins <bfins@alunos.isel.ipl.pt>
2015-01-08 14:37:17 +0000

    LCD

--- se1/sources/Lab6/LCD.c
+++ se1/sources/Lab6/LCD.c
@@ -6,16 +6,25 @@ unsigned int data_mask = 0x00000F00;
 unsigned int rs_mask = 0x00002000;
 unsigned int enable_mask = 0x00001000;
 
+void setEnable(){
+    GPIO_output(enable_mask,enable_mask);
+}
+
+void resetEnable(){
+    GPIO_output(0,enable_mask);
+}
+
 void write_Nibble(int rs, unsigned int data){
-	GPIO_output(enable_mask,enable_mask);
-	rs = rs? rs_mask :0;
+	rs = rs? rs_mask : 0;
 	GPIO_output(rs,rs_mask);
 	GPIO_output(data<<8, data_mask);
+    setEnable();
+    resetEnable();
 }
 
 void write_Byte(int rs, int c){
-		write_Nibble(rs,c >> 4);
-		write_Nibble(rs,c);
+	write_Nibble(rs,(c & 0xF0) >> 4);
+	write_Nibble(rs,(c & 0x0F));
 }
 
 /* Faz a iniciação do sistema para permitir o acesso ao periférico
@@ -40,7 +49,7 @@ void LCD_Init(void){
 
 /* Escreve um carácter na posição corrente do cursor. */
 void LCD_WriteChar(char ch){
-		write_Byte(1,ch);
+	write_Byte(1,ch);
 }
 
 /* Escreve uma string na posição corrente do cursor. */
@@ -53,8 +62,8 @@ void LCD_WriteString(char *str){
 
 /* Posiciona o cursor na linha x e coluna y do mostrador. */
 void LCD_Goto(int x, int y){
-		write_Byte(0,(y==0)? 0x80+x:0xC0+x );
-		//time.sleep(2);
+	write_Byte(0,(y==0)? 0x80+x:0xC0+x );
+	//time.sleep(2);
 }
 
 /* Limpa o visor, usando o comando disponível na API do periférico. */
