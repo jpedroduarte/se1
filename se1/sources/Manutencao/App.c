ead57f1a08532f89ba5c61a537c78ac1d2240b82
brunofins <bfins@alunos.isel.ipl.pt>
2015-01-17 21:31:04 +0000

    Manager

--- se1/sources/Manutencao/App.c
+++ se1/sources/Manutencao/App.c
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 #include "/home/user/Desktop/host-se1/se1/includes/Button.h"
 #include "Menu.h"
@@ -18,18 +15,33 @@ int main(){
 	Button bOk =  BUTTON_Init(4);
 	
 	Menu m = {sbm};
-	Manager_Print(&m);
+	Menu *pm = &m;
+	
+	LCD_WriteString("MODO DE");
+	LCD_Goto(1,6);
+	LCD_WriteString("Manutencao");
+	TMR0_Delay(3000);
+
+	Manager_Print(pm);
 
 	while(1){
+		if(BUTTON_getState(bOk)){
+			if(Manager_Entry(pm))
+				break;
+		}
+		
 		if(BUTTON_getState(bUp)){
-			MENU_Navigation(&m,UP);
-			Manager_Print(&m);
+			MENU_Navigation(pm,UP);
+			Manager_Print(pm);
 			TMR0_Delay(1000);
 		}
 		if(BUTTON_getState(bDown)){
-			MENU_Navigation(&m,DOWN);
-			Manager_Print(&m);
+			MENU_Navigation(pm,DOWN);
+			Manager_Print(pm);
 			TMR0_Delay(1000);
 		} 
-	}	
+	}
+	LCD_Clear();
+	LCD_WriteString("MODO NORMAL");
+	while(1);	
 }
--- se1/sources/Manutencao/Manager.c
+++ se1/sources/Manutencao/Manager.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "Manager.h"
 #include "/home/user/Desktop/host-se1/se1/includes/lcd.h"
 
@@ -16,5 +19,57 @@ char* Manager_getStringMenu(int curr, int line){
 
 ManagerInfo* Manager_getMenu(int curr){
 	return ((ManagerInfo*)pMenus)+curr;
-	
+}
+
+int Manager_Entry(Menu *m){
+	int curr = MENU_getCurr(m)-1;
+	ManagerInfo* ptr = Manager_getMenu(curr);
+	EntryMode e = ptr->enter;
+	e.on = 1;
+	Manager_Enter(e);
+	if(e.mType != EXIT){
+		Manager_Print(m);
+		return 0; //CONTINUE IN MANAGER
+	}
+	return 1; //EXIT MANAGER
+}
+
+void Manager_Enter(EntryMode e){
+	ManType mT = e.mType;
+	int x = 1;
+	int y = 6;
+	//int x = Manager_EntryX(e);
+	//	int y = Manager_EntryY(e);
+	switch(mT){
+		case SET:
+			
+			LCD_Goto(x,y);
+			TMR0_Delay(5000);
+			break;
+		case LOG:
+			LCD_Clear();
+			LCD_WriteString("LOG TEMP******");
+			TMR0_Delay(5000);
+			
+			break;
+		case EXIT:
+			LCD_Clear();
+			LCD_WriteString("EXIT");
+			TMR0_Delay(5000);
+			break;
+		default:
+			break;
+	}
+}
+
+void n2str(char * str, int n, int p){
+	*(str + p) = '0'+(n/10);
+	*(str + p + 1) = ('0' +(n%10));
+}
+
+void putYear(char * str, int n, int p){
+	*(str + p) = '0'+(n/1000);
+	*(str + p +1) = '0'+((n%1000)/100);
+	*(str + p + 2) = '0'+(((n%1000)%100)/10);
+	*(str + p + 3) = ('0' +(n%10));
 }
