926bb250b60b53e509826becdcb14bae312f7c91
smccms <susanamcardoso94@gmail.com>
2015-01-18 00:24:35 +0000

    Button api

--- se1/includes/Button.h
+++ se1/includes/Button.h
@@ -1,14 +1,36 @@
+/** @file Button.h
+*  Button module header.
+*/
 #ifndef BUTTON_H
 #define BUTTON_H
 
 #include "/home/user/Desktop/host-se1/se1/includes/gpio.h"
-
+/**
+ * Estrutura que define um botao
+ */
 typedef struct Button{
 	int pinMask;
+	enum state currentState;
 } Button;
 
-Button BUTTON_Init(int pinId);
+/**
+ * Cria um novo botao atribuido a um pin
+ * @param pinId Indica o pin a qual o botao esta ligado
+ * @return retorna um novo botao
+ */
+Button Button_Init(int pinId);
 
-int BUTTON_getState(Button b);
+/**
+ * Devolve o valor que se encontra no pin daquele botao
+ * @param b Butao da qual se pretende saber o estado do porto
+ * @return retorna 0 ou 1
+ */
+int Button_getPortState(Button b);
+
+/**
+ * Avalia e altera o estado do botao
+ * @param b Butao da qual se pretende avaliar o estado
+ */
+void Button_setState(Button b);
 
 #endif
